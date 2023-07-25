ecc7aa42f1f63643c1d6f69beee36db5e24906a9
kagunecode <cesar.acb98@gmail.com>
2023-07-25 15:29:59 -0500

    Added the whole game logic, as well as the UI
    Still need to  ake some changes to make the UI look better.
    Still checking for bugs.

--- Projects/2_rock_paper_scissors_js/script.js
+++ Projects/2_rock_paper_scissors_js/script.js
@@ -7,6 +7,7 @@ function getComputerChoice() {
 function getResult(playerChoice, computerChoice) {
     playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase()
     console.log(`The CPU played ${computerChoice}`)
+    console.log(`The user played ${playerChoice}`)
     if (playerChoice !== "Rock" && playerChoice !== "Paper" && playerChoice !== "Scissors") {
         return `${playerChoice} is not a valid option. Options are rock, paper or scissors`
     } else if (playerChoice === computerChoice) {
@@ -20,8 +21,8 @@ function getResult(playerChoice, computerChoice) {
     }    
 }
 
-function playGame(userName) {
-    window.alert(`Welcome ${userName}! You will play 5 times against the CPU. Good luck!`)
+/*function playGame(userName) {
+    window.alert(`Welcome ${userName}! You will play against the CPU. Good luck!`)
     let userScore = 0;
     let cpuScore = 0;
     let draw = 0;
@@ -38,4 +39,45 @@ function playGame(userName) {
     console.log(result)
     console.log(`User: ${userScore}   ||  CPU: ${cpuScore}    ||    Draws: ${draw}`)
     }
+} */
+
+function scoreCounter(result, userScore, cpuScore, drawScore, userDiv, cpuDiv, drawDiv) {`button[data-value]`
+    if(result.includes('won')) {
+        userScore += 1;
+        userDiv.innerText = userScore;
+        return 'user won'
+    } else if (result.includes('lose')) {
+        cpuScore += 1;
+        cpuDiv.innerText = cpuScore;
+        return 'cpu won'
+    } else {
+        drawScore += 1;
+        drawDiv.innerText = drawScore;
+        return 'draw'
+    }
 }
+
+let userScore = 0;
+let cpuScore= 0;
+let drawScore = 0;
+const userButton = document.querySelectorAll('#button-choice');
+const resultDiv = document.querySelector('#result');
+const userCount = document.querySelector('#user');
+const cpuCount = document.querySelector('#cpu');
+const drawCount = document.querySelector('#draws');
+const headerText = document.querySelector('.header-title');
+
+userButton.forEach(button => button.addEventListener('click', (e) =>{
+    computer = getComputerChoice();
+    game = getResult(e.target.innerText, computer);
+    result = scoreCounter(game, userScore, cpuScore, drawScore, userCount, cpuCount, drawCount);
+    resultDiv.innerText = game;
+    headerText.innerText = `The CPU played ${computer}` ;
+    if (result.includes('user won')) {
+        userScore += 1;
+    } else if (result.includes('cpu won')) {
+        cpuScore += 1;
+    } else {
+        drawScore += 1;
+    }
+}))
